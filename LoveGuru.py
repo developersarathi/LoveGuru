a52b245fe0119f1a9dd8db1a9f37840d79c28b53
developersarathi <68042722+developersarathi@users.noreply.github.com>
2020-07-09 08:57:35 +0530

    Add files via upload

---
+++ LoveGuru.py
@@ -0,0 +1,27 @@
+#send it to your crush
+
+
+
+import time
+
+print("Hey!")
+time.sleep(1)
+print("Coded by SS4LYF")
+time.sleep(1)
+name2=input("Tell me Name of person who sended you this:")
+print("I can tell you how much "+name2+" Loves you")
+time.sleep (2)
+print("Interesting!!")
+time.sleep(1)
+print("Ok,so let's get started")
+name1=input("Your Name:")
+print("Type 'yes' to continue")
+
+x=str(input("Do You want this to continue:"))
+if x=="yes":
+  while True:
+    time.sleep(.5)
+    print("Dear "+name1 +","+name2+"'s"+" Love for you is Limitless!")
+else:
+  print("As you wish!")
+  print("You rejected "+name2+" proposal")
